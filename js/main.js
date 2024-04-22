f531ef8c637607657aac041a1f4e90a6e0770dde
mauricioti23 <mauriciosv09@gmail.com>
2024-04-22 16:32:21 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$('#telefone').mask('(00) 00000-0000', {
+    placeholder: '(XX) XXXXX-XXXX'
+})
+
+$('#cpf').mask('000.000.000-00', {
+    placeholder: 'XXX.XXX.XXX-XX'
+})
+
+$('#cep').mask('00000-000', {
+    placeholder: 'XXXXX-XXX'
+})
